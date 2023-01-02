07a8e300714436c6c390da4ef683a39f1fb65651
dcamilovelandia <deybi.velandia@bodytechcorp.com>
2023-01-02 12:12:52 -0500

    deleted google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:animations/clock_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,10 +18,6 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (_, __) => MaterialApp(
         title: 'Flutter Demo',
-        theme: Theme.of(context).copyWith(
-          textTheme: GoogleFonts.pressStart2pTextTheme(),
-          scaffoldBackgroundColor: Colors.black12
-        ),
         home: ClockScreen()
       ),
     );
